541515af394dfb97cf6e0d904be2e4fd5636e773
BinhTransx25 <binhthaps36193@fpt.edu.vn>
2024-01-19 01:22:52 +0700

    màn hình hot spots

---
+++ constants/icons.js
@@ -0,0 +1,6 @@
+export default {
+    search: require('../assets/images/loupe.png'),
+    muiTen: require('../assets/images/muiTen.png'),
+    menu: require('../assets/images/app.png'),
+
+}
---
+++ src/FoodooItem.js
@@ -0,0 +1,117 @@
+import {
+    Text, View, Image, ImageBackground,
+    TouchableOpacity, TextInput, KeyboardAvoidingView,
+    Keyboard, Platform, ScrollView, FlatList, StyleSheet
+} from 'react-native';
+import React, { useState, useEffect, } from 'react';
+import { images, icons, } from '../constants';
+import { UIButton } from '../components';
+import { Alert } from 'react-native';
+const FoodooItem = (props) => {
+    let { name, price, url } = props.foods
+    const { onPress } = props
+    return (
+        <TouchableOpacity
+            onPress={onPress}
+            style={styles.container}>
+            <View style={styles.Class3}>
+                <View style={styles.item}>
+                    <Image source={{ uri: url }}
+                        style={styles.imageItem} />
+                    <View style={styles.ClassChild}>
+                        <Text style={styles.nameItem}>{name}</Text>
+                        <Text style={styles.priceItem}> <Text style={styles.dollar}>$</Text> {price}</Text>
+                    </View>
+                </View>
+            </View>
+        </TouchableOpacity>
+
+    )
+}
+export default FoodooItem
+const styles = StyleSheet.create({
+    dollar: { color: '#FFC532' },
+    ClassChild: {
+        alignItems: 'center', marginTop: 70
+    },
+    imageItem: {
+        width: '100%',
+        height: '110%',
+        position: 'absolute',
+        bottom: 60,
+        resizeMode: 'contain',
+
+    },
+    priceItem: {
+        fontWeight: 'bold',
+        lineHeight: 35,
+        fontSize: 15
+    },
+    nameItem: {
+        fontWeight: 'bold',
+        lineHeight: 15,
+        fontSize: 15,
+    },
+    item: {
+        height: 133,
+        width: 159,
+        backgroundColor: 'white',
+        borderRadius: 8,
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        shadowRadius: 4,
+        elevation: 8,
+        marginBottom: 20,
+
+    },
+    Class3: {
+        flexDirection: 'row',
+        marginTop: 80,
+        flex: 1
+
+    },
+    titleInput: {
+        marginLeft: 60
+    },
+    iconMenu: {
+        height: 30,
+        width: 30,
+        marginLeft: 100,
+        marginTop: 12
+    },
+    iconSearch: {
+        position: 'absolute',
+        height: 26,
+        width: 26,
+        marginLeft: 25,
+        marginTop: 12
+    },
+    Class2: {
+        backgroundColor: '#E5E5E575',
+        flexDirection: 'row',
+        marginTop: 25,
+        width: 353,
+        height: 54,
+        borderRadius: 30
+    },
+    icon: {
+        right: 230
+    },
+    title: {
+        fontSize: 28,
+        fontWeight: 'bold',
+        color: '#000000',
+    },
+    container: {
+        alignItems: 'center',
+        flex: 1,
+
+    },
+    Class1: {
+        flexDirection: 'row',
+        marginTop: 40,
+        justifyContent: 'center'
+
+    }
+})
